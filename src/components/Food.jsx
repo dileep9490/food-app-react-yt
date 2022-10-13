9e3fb005e90eb824a07cde687d876a2099d955ff
dileep <dileepgudimetla9490@gmail.com>
2022-10-13 14:22:20 +0530

    filter added

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -4,6 +4,20 @@ import { data } from "../data/data";
 const Food = () => {
 	const [foods, setFoods] = useState(data);
 
+	// filter by type
+	const filterType = (category) => {
+		setFoods(data.filter((item)=>{
+            return item.category === category
+        }))
+	};
+
+    // filter by price
+    const filterPrice = (price) =>{
+        setFoods(data.filter((item)=>{
+            return item.price === price
+        }))
+    }
+
 	return (
 		<div className="max-w-1=[1640px] mx-auto px-4 py-12">
 			<h1 className="text-orange-600 font-bold text-4xl text-center">
@@ -15,19 +29,19 @@ const Food = () => {
 				<div>
 					<p className="font-bold text-gray-700">Filter Type</p>
 					<div className="flex justify-between flex-wrap">
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>setFoods(data)} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							All
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterType('burger')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							Burgers
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterType('pizza')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							Pizzas
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterType('salad')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							Salads
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterType('chicken')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							Chicken
 						</button>
 					</div>
@@ -37,16 +51,16 @@ const Food = () => {
 				<div>
 					<p className="font-bold text-gray-700">Filter Price</p>
 					<div className="flex justify-between max-w-[390px] ">
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterPrice('$')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							$
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterPrice('$$')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							$$
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterPrice('$$$')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							$$$
 						</button>
-						<button className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
+						<button onClick={()=>filterPrice('$$$$')} className="border-orange-600 text-orange-600 hover:text-white hover:bg-orange-600 m-1">
 							$$$$
 						</button>
 					</div>
@@ -59,11 +73,17 @@ const Food = () => {
 						key={index}
 						className="border shadow-lg hover:scale-105 duration-300 rounded-lg"
 					>
-						<img  className="w-full h-[200px] object-cover rounded-t-lg" src={item.image} alt={item.name} />
+						<img
+							className="w-full h-[200px] object-cover rounded-t-lg"
+							src={item.image}
+							alt={item.name}
+						/>
 						<div className="flex justify-between px-2 py-4">
 							<p className="font-bold">{item.name}</p>
 							<p>
-								<span className="bg-orange-600 rounded-full text-white p-1">{item.price}</span>
+								<span className="bg-orange-600 rounded-full text-white p-1">
+									{item.price}
+								</span>
 							</p>
 						</div>
 					</div>
